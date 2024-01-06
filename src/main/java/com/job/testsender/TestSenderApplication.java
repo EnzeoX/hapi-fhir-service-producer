5091c6b0e3e3f2603a96712236193947bbdea256
Boyko Nikolay <enzeox@gmail.com>
2024-01-07 00:10:21 +0200

    excluded configuration classes

--- src/main/java/com/job/testsender/TestSenderApplication.java
+++ src/main/java/com/job/testsender/TestSenderApplication.java
@@ -2,8 +2,16 @@ package com.job.testsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        SecurityAutoConfiguration.class})
 public class TestSenderApplication {
 
     public static void main(String[] args) {

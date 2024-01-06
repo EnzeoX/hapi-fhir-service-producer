160a2a014f1ddfe45a4fe575b74176d18970f84e
Boyko Nikolay <enzeox@gmail.com>
2024-01-07 00:07:03 +0200

    Added simple token wrapper

---
+++ src/main/java/com/job/testsender/utils/TokenWrapper.java
@@ -0,0 +1,28 @@
+package com.job.testsender.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@Component
+public class TokenWrapper {
+
+    public final static String BEARER = "Bearer ";
+    public final static String AUTH_HEADER = "Authorization";
+
+    private static final Map<String, String> contextPathTokens = new HashMap<>();
+
+    static {
+        contextPathTokens.put("/api/v1/process-bundle", "ProvidedTokenForBundleProcessing");
+    }
+
+    public String getTokenForPath(@NotNull String contextPath) {
+        return contextPathTokens.get(contextPath);
+    }
+}

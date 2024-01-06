b72fb85aaa0fdd2a0f42917eca5bec79b8e61718
Boyko Nikolay <enzeox@gmail.com>
2024-01-07 00:05:57 +0200

    Added simple bearer token interceptor

--- src/main/java/com/job/testsender/config/interceptor/TokenInterceptor.java
+++ src/main/java/com/job/testsender/config/interceptor/TokenInterceptor.java
@@ -1,25 +1,36 @@
 package com.job.testsender.config.interceptor;
 
+import com.job.testsender.utils.TokenWrapper;
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.file.AccessDeniedException;
 import java.util.Objects;
 
+import static com.job.testsender.utils.TokenWrapper.AUTH_HEADER;
+import static com.job.testsender.utils.TokenWrapper.BEARER;
+
+@Slf4j
+@AllArgsConstructor
 public class TokenInterceptor implements HandlerInterceptor {
 
-    private final static String BEARER = "Bearer ";
-    private final static String AUTH_HEADER = "Authorization";
-    private final static String TOKEN = "TopSecretBearerToken";
+    private final TokenWrapper tokenWrapper;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        if (Objects.requireNonNull(request.getHeader(AUTH_HEADER), "No auth header provided").startsWith(BEARER)) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws AuthenticationException {
+        if (request.getHeader(AUTH_HEADER).length() > 6) {
             String requestToken = request.getHeader(AUTH_HEADER).substring(BEARER.length());
-            return requestToken.equals(TOKEN);
+            if (Objects.requireNonNull(tokenWrapper.getTokenForPath(request.getServletPath()), "No token for provided URI")
+                    .equals(requestToken)) {
+                return true;
+            }
+            throw new AuthenticationException("Illegal token provided");
         }
-
-        return false;
+        throw new NullPointerException("Provided authorization header is null or empty");
     }
 }

3ae5d46d0310a3b53c14b8da75e6bca88b1bef89
Boyko Nikolay <enzeox@gmail.com>
2024-01-07 14:49:40 +0200

    Deleted commented code

--- src/main/java/com/job/testsender/config/AppConfig.java
+++ src/main/java/com/job/testsender/config/AppConfig.java
@@ -8,15 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class AppConfig {
 
-//    @Bean
-//    public FilterRegistrationBean<UrlBearerFilter> controllerFilter() {
-//        FilterRegistrationBean<UrlBearerFilter> controllerFilter = new FilterRegistrationBean<>();
-//        controllerFilter.setFilter(new UrlBearerFilter());
-//        controllerFilter.addUrlPatterns("/api/v1/process-bundle");
-//        controllerFilter.setOrder(1);
-//        return controllerFilter;
-//    }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();

2032ea7b6651ca9ec049fcc1fc409ce6252eff4b
Boyko Nikolay <enzeox@gmail.com>
2024-01-08 13:20:39 +0200

    Removed condition option, changed user id type from String to Long User class now implements UserDetails

--- src/main/java/com/job/testsender/entity/User.java
+++ src/main/java/com/job/testsender/entity/User.java
@@ -1,32 +1,81 @@
 package com.job.testsender.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.apache.logging.log4j.message.AsynchronouslyFormattable;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name = "users")
-public class User implements Serializable {
+@AllArgsConstructor()
+@NoArgsConstructor
+public class User implements Serializable, UserDetails {
+
 
     @Id
-    @GeneratedValue
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @Column(name = "username")
     private String username;
+
     @Column(name = "password")
     private String password;
 
     public User(String username, String password) {
-        this.username = username;
         this.password = password;
+        this.username = username;
+    }
+
+//    @Column(name = "email")
+//    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public enum Role {
+        UNAUTHORIZED,
+        USER,
+        ADMIN
     }
 }
--- src/main/java/com/job/testsender/repository/UserRepository.java
+++ src/main/java/com/job/testsender/repository/UserRepository.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-@ConditionalOnExpression("${security.jwt.enabled} and not ${security.simple-token.enabled}")
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 }

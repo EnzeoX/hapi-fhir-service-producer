208c013d8f960c6aea336077bc8bb7e9a4d21153
Boyko Nikolay <enzeox@gmail.com>
2024-01-08 13:21:31 +0200

    Simplified userService, added method to validate and save user

--- src/main/java/com/job/testsender/service/UserService.java
+++ src/main/java/com/job/testsender/service/UserService.java
@@ -1,37 +1,33 @@
 package com.job.testsender.service;
 
 import com.job.testsender.entity.User;
-import com.job.testsender.exception.UserNotFoundException;
 import com.job.testsender.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
 @Slf4j
 @Service
 @AllArgsConstructor
-@ConditionalOnExpression("${security.jwt.enabled} and not ${security.simple-token.enabled}")
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Objects.requireNonNull(username, "Username is null or empty");
-        User user = this.userRepository.findByUsername(username).orElseThrow();
-        List<String> roles = List.of("USER");
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .roles(roles.toArray(new String[0]))
-                .build();
+    public UserDetails loadUserByUsername(@NotBlank(message = "Username is null or empty") String username) throws UsernameNotFoundException {
+        return this.userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public void saveNewUser(User user) {
+        Objects.requireNonNull(user, "Provided user model is null");
+        userRepository.save(user);
     }
 }

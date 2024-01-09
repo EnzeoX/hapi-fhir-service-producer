d874fd5000feba17a346578ebdf59a99c4bee7f8
Boyko Nikolay <enzeox@gmail.com>
2024-01-09 12:01:34 +0200

    Model for error response

---
+++ src/main/java/com/job/testsender/model/GlobalErrorResponse.java
@@ -0,0 +1,12 @@
+package com.job.testsender.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GlobalErrorResponse {
+
+    private String status;
+    private String message;
+}

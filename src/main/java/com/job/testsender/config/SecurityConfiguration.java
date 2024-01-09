08e64bbcb6fe1e29b031ca95e6cb79619b8d83c1
Boyko Nikolay <enzeox@gmail.com>
2024-01-09 12:01:04 +0200

    Added filter exception handler

--- src/main/java/com/job/testsender/config/SecurityConfiguration.java
+++ src/main/java/com/job/testsender/config/SecurityConfiguration.java
@@ -1,68 +1,49 @@
 package com.job.testsender.config;
 
 import com.job.testsender.config.interceptor.JwtAuthenticationFilter;
-import com.job.testsender.service.UserService;
+import com.job.testsender.config.interceptor.FilterException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class SecurityConfiguration {
 
-    private final UserService userService;
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userService);
-        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            FilterException filterException) throws Exception {
+        http //disable csrf
                 .csrf()
-                .disable()
+                .disable();
+        http //set permissions
                 .authorizeRequests()
-                .antMatchers("/api/v1/authentication/auth").hasAnyRole()
-                .antMatchers("/api/v1/authentication/register").hasAnyRole()
+                .antMatchers(HttpMethod.GET,"/api/v1").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/v1/authentication/**").permitAll()
                 .anyRequest()
                 .authenticated()
-                .and()
+                .and();
+        http //set session management
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .and();
+        http // add jwt filter
                 .authenticationProvider(authenticationProvider)
+                .addFilterBefore(filterException, LogoutFilter.class)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }

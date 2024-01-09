c4f18c880c8f5c7e2d4525f5afb9baf50d6c6f74
Boyko Nikolay <enzeox@gmail.com>
2024-01-09 12:00:51 +0200

    Added configuration beans for (Moved from SecurityConfiguration.class)  AuthenticationManager, AuthenticationProvider, BCryptPasswordEncoder)

--- src/main/java/com/job/testsender/config/AppConfig.java
+++ src/main/java/com/job/testsender/config/AppConfig.java
@@ -1,14 +1,34 @@
 package com.job.testsender.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import com.job.testsender.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
+    @Bean
+    public AuthenticationProvider authenticationProvider(BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return authenticationProvider;
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }

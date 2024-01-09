52ab406f1a0ea65cc27aa3bb1a9d39e13d4283c1
Boyko Nikolay <enzeox@gmail.com>
2024-01-09 12:04:15 +0200

    Code style changed

--- src/main/java/com/job/testsender/model/RegistrationRequest.java
+++ src/main/java/com/job/testsender/model/RegistrationRequest.java
@@ -1,14 +1,13 @@
 package com.job.testsender.model;
 
-import lombok.*;
-
-import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegistrationRequest {
-
     private String username;
     private String password;
 }

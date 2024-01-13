3b25db167292f1f4d051a04c900d67f73a557d2e
EnzeoX <enzeox@gmail.com>
2024-01-13 20:45:48 +0200

    Added auth exception handler

--- src/main/java/com/job/testsender/config/SecurityConfiguration.java
+++ src/main/java/com/job/testsender/config/SecurityConfiguration.java
@@ -1,16 +1,18 @@
 package com.job.testsender.config;
 
-import com.job.testsender.config.interceptor.JwtAuthenticationFilter;
 import com.job.testsender.config.interceptor.FilterException;
+import com.job.testsender.config.interceptor.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
@@ -45,6 +47,11 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(filterException, LogoutFilter.class)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                );
         return http.build();
     }
 }

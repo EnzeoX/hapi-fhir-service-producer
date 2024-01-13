372b1969ef58d1cb76902de2df721178c030cc91
EnzeoX <enzeox@gmail.com>
2024-01-13 20:43:58 +0200

    Added non null requirement

--- src/main/java/com/job/testsender/service/AuthService.java
+++ src/main/java/com/job/testsender/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.job.testsender.service;
 
+import com.job.testsender.exception.UnauthorizedUserException;
 import com.job.testsender.handler.AuthHandler;
 import com.job.testsender.model.AuthRequest;
 import com.job.testsender.model.AuthResponse;
@@ -7,6 +8,8 @@ import com.job.testsender.model.RegistrationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -16,10 +19,14 @@ public class AuthService {
     private final AuthHandler authHandler;
 
     public AuthResponse register(RegistrationRequest registration) {
+        Objects.requireNonNull(registration, "No registration data provided");
         return authHandler.registerUser(registration);
     }
 
     public AuthResponse authenticate(AuthRequest login) {
-        return authHandler.authenticateUser(login);
+        Objects.requireNonNull(login, "No login data provided");
+        AuthResponse response = authHandler.authenticateUser(login);
+        if (response == null) throw new UnauthorizedUserException("User not found");
+        return response;
     }
 }

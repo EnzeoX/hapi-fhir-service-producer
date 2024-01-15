5ee59aa3eb69ddeaeaa6a79daff8fb440c5f90fe
Boyko Nikolay <enzeox@gmail.com>
2024-01-15 15:15:28 +0200

    JwtUtil tests

--- src/test/java/com/job/testsender/util/JwtUtilTest.java
+++ src/test/java/com/job/testsender/util/JwtUtilTest.java
@@ -7,13 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
-@SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtUtilTest {
 
@@ -21,12 +19,19 @@ public class JwtUtilTest {
 
     private final String totallyExpiredToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUZXN0IiwiaWF0IjoxNzA0ODM5NzU2LCJleHAiOjE3MDQ4NDMzNTZ9.Q47YXq9VCmv4lvfEvBiHCP8S-V5qEbggZmrbDRIS57g";
 
-    @Autowired
     private JwtUtils jwtUtils;
 
     @BeforeAll
     public void init() {
         mockedUser = new User("TEST", "TEST");
+        jwtUtils = new JwtUtils();
+        ReflectionTestUtils.setField(jwtUtils,
+                "SECRET_KEY",
+                "23576d72294b643f624e6d3a737c2c7561633942306f64334c644e365f415e5f");
+        ReflectionTestUtils.setField(jwtUtils,
+                "accessTokenValidity",
+                3600000);
+
     }
 
     @Test

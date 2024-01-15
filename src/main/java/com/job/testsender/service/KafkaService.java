d16910e3d5932a99237d7f5c08b4ef78bdc7aac1
Boyko Nikolay <enzeox@gmail.com>
2024-01-15 15:16:07 +0200

    Renamed from KafkaProducer Added validations

--- src/main/java/com/job/testsender/service/KafkaService.java
+++ src/main/java/com/job/testsender/service/KafkaService.java
@@ -6,31 +6,28 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @Service
-public class KafkaProducer {
+public class KafkaService {
 
     @Value(value = "${application.kafka.topic}")
     private String kafkaTopic;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendKafkaMessage(String message) {
-        Objects.requireNonNull(message, "Provided Kafka message for sending is null or empty");
+        if (message == null || message.isEmpty()) throw new NullPointerException("Provided Kafka message for sending is null or empty");
         this.kafkaTemplate.send(kafkaTopic, message);
     }
 
     public void sendMultipleKafkaMessages(List<String> data) {
-        Objects.requireNonNull(data, "Provided list of elements is null or empty");
-        data.forEach(obj -> {
-            log.info(obj);
-            sendKafkaMessage(obj);
-        });
+        if (data == null || data.isEmpty()) throw new NullPointerException("Provided list of elements is null or empty");
+        log.info(String.valueOf(data.size()));
+        data.forEach(this::sendKafkaMessage);
     }
 }

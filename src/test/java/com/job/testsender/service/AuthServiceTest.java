78885ecba4f0aeafc587b9666f19a9244d8d200f
Boyko Nikolay <enzeox@gmail.com>
2024-01-15 15:13:36 +0200

    AuthService tests

--- src/test/java/com/job/testsender/service/AuthServiceTest.java
+++ src/test/java/com/job/testsender/service/AuthServiceTest.java
@@ -1,9 +1,9 @@
 package com.job.testsender.service;
 
-import com.job.testsender.entity.User;
 import com.job.testsender.exception.UnauthorizedUserException;
 import com.job.testsender.handler.AuthHandler;
 import com.job.testsender.model.AuthRequest;
+import com.job.testsender.model.AuthResponse;
 import com.job.testsender.model.RegistrationRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -36,14 +37,13 @@ public class AuthServiceTest {
 
     @Test
     public void testRegister_null_data_provided() {
-        assertThrows(NullPointerException.class, () -> authService.register(null));
+        NullPointerException nullEx = assertThrows(NullPointerException.class, () -> authService.register(null));
+        assertEquals("No registration data provided", nullEx.getMessage());
     }
 
     @Test
     public void testAuthenticate() {
-        RegistrationRequest request = new RegistrationRequest("username", "password");
-        authService.register(request);
-
+        when(authHandler.authenticateUser(any())).thenReturn(new AuthResponse("token"));
         AuthRequest login = new AuthRequest("username", "password");
         authService.authenticate(login);
         verify(authHandler).authenticateUser(login);
@@ -51,13 +51,14 @@ public class AuthServiceTest {
 
     @Test
     public void testAuthenticate_null_data_provided() {
-        assertThrows(NullPointerException.class, () -> authService.authenticate(null));
+        NullPointerException nullEx = assertThrows(NullPointerException.class, () -> authService.authenticate(null));
+        assertEquals("No login data provided", nullEx.getMessage());
     }
 
     @Test
     public void testAuthenticate_no_user_found() {
-        assertThrows(UnauthorizedUserException.class,
-                () -> authService.authenticate(new AuthRequest("T", "T")),
-                "User not found");
+        UnauthorizedUserException authEx = assertThrows(UnauthorizedUserException.class,
+                () -> authService.authenticate(new AuthRequest("T", "T")));
+        assertEquals("User not found", authEx.getMessage());
     }
 }

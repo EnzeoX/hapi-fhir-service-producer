192c7d7eb9eb599981f7b1354c87ec2b486c411a
Boyko Nikolay <enzeox@gmail.com>
2024-01-15 15:33:58 +0200

    Removed unused imports

--- src/main/java/com/job/testsender/TestSenderApplication.java
+++ src/main/java/com/job/testsender/TestSenderApplication.java
@@ -2,10 +2,6 @@ package com.job.testsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class TestSenderApplication {

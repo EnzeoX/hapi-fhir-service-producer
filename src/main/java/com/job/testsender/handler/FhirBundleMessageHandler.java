b09a88f3a13b420e872fb15759a71ee773c0a56c
Boyko Nikolay <enzeox@gmail.com>
2024-01-15 15:14:15 +0200

    Added value validation for null and empty

--- src/main/java/com/job/testsender/handler/FhirBundleMessageHandler.java
+++ src/main/java/com/job/testsender/handler/FhirBundleMessageHandler.java
@@ -2,37 +2,38 @@ package com.job.testsender.handler;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
-import com.job.testsender.service.KafkaProducer;
+import com.job.testsender.service.KafkaService;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class FhirBundleMessageHandler {
 
-    private final KafkaProducer kafkaProducer;
+    private final KafkaService kafkaService;
 
     private final IParser parser;
 
-    public FhirBundleMessageHandler(KafkaProducer kafkaProducer) {
-        this.kafkaProducer = kafkaProducer;
+    public FhirBundleMessageHandler(KafkaService kafkaService) {
+        this.kafkaService = kafkaService;
         this.parser = FhirContext.forR4().newJsonParser().setPrettyPrint(true);
     }
 
-    public void collectAndProcessBundle(@NotNull(message = "Provided bundle message is null or empty") String message) {
-
+    public void collectAndProcessStringBundle(String message) {
+        if (message == null || message.isEmpty()) throw new NullPointerException("Provided bundle string is null or empty");
         Bundle bundle = parser.parseResource(Bundle.class, message);
         collectAndProcessBundle(bundle);
     }
 
-    public void collectAndProcessBundle(@NotNull Bundle bundle) {
+    public void collectAndProcessBundle(Bundle bundle) {
+        Objects.requireNonNull(bundle, "Provided bundle object is null");
         List<String> finishedEncounterStringStream = bundle.getEntry().stream()
                 .filter(entry -> entry.getResource().getResourceType() == ResourceType.Encounter)
                 .map(entry -> (Encounter) entry.getResource())
@@ -47,7 +48,7 @@ public class FhirBundleMessageHandler {
                 .collect(Collectors.toList());
         if (finishedEncounterStringStream.size() > 0) {
             log.info("Encounter list contains elements, processing");
-            kafkaProducer.sendMultipleKafkaMessages(finishedEncounterStringStream);
+            kafkaService.sendMultipleKafkaMessages(finishedEncounterStringStream);
         }
     }
 

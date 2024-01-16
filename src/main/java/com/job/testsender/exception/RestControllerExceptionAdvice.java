11a80355511ef362f3f0ead80ec5ff15b9e73bcd
EnzeoX <enzeox@gmail.com>
2024-01-16 21:27:40 +0200

    Added DataIntegrityViolationException handling

--- src/main/java/com/job/testsender/exception/RestControllerExceptionAdvice.java
+++ src/main/java/com/job/testsender/exception/RestControllerExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.job.testsender.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,13 +19,16 @@ public class RestControllerExceptionAdvice {
             case "StringIndexOutOfBoundsException":
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Provided incorrect string");
             case "NullPointerException":
-            case "DataFormatException" :
+            case "DataFormatException":
             case "IllegalArgumentException":
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
             case "HttpMessageNotReadableException":
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Required request body is missing");
             case "AuthenticationException":
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+            case "DataIntegrityViolationException":
+                ConstraintViolationException throwable = (ConstraintViolationException) e.getCause();
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(throwable.getCause().getMessage());
             default:
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }

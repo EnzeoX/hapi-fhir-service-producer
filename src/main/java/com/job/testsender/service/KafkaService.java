b5708a5889bad82d4043fb9c497f9f70be623685
EnzeoX <enzeox@gmail.com>
2024-01-17 18:41:19 +0200

    Added Kafka connection error handler

--- src/main/java/com/job/testsender/service/KafkaService.java
+++ src/main/java/com/job/testsender/service/KafkaService.java
@@ -2,7 +2,9 @@ package com.job.testsender.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.event.NonResponsiveConsumerEvent;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,13 +23,22 @@ public class KafkaService {
     }
 
     public void sendKafkaMessage(String message) {
-        if (message == null || message.isEmpty()) throw new NullPointerException("Provided Kafka message for sending is null or empty");
+        if (message == null || message.isEmpty())
+            throw new NullPointerException("Provided Kafka message for sending is null or empty");
+        log.info("Sending message {} to kafka", message);
         this.kafkaTemplate.send(kafkaTopic, message);
     }
 
     public void sendMultipleKafkaMessages(List<String> data) {
-        if (data == null || data.isEmpty()) throw new NullPointerException("Provided list of elements is null or empty");
-        log.info(String.valueOf(data.size()));
+        if (data == null || data.isEmpty())
+            throw new NullPointerException("Provided list of elements is null or empty");
         data.forEach(this::sendKafkaMessage);
+        log.info("Messages sent");
+    }
+
+    @EventListener()
+    public void eventHandler(NonResponsiveConsumerEvent event) {
+        //When Kafka server is down, NonResponsiveConsumerEvent error is caught here.
+        log.warn("CAUGHT the event " + event);
     }
 }
